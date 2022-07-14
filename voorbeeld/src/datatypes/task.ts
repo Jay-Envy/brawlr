9e18e5de61586eb7d13737b8969788f22203e686
Jay-Envy <74972940+Jay-Envy@users.noreply.github.com>
2022-07-14 12:07:27 +0200

    les1 + les2

---
+++ voorbeeld/src/datatypes/task.ts
@@ -0,0 +1,22 @@
+export interface ITask{
+    name: string;
+    id: number;
+    done: boolean;
+}
+
+//als je type methodes nodig heeft is een klasse een betere keuze
+//om te communiceren met een database (data die je terugkrijgt manipuleren en terugsturen) moet je methodes toevoegen aan je klasse
+//deze kunnen niet geconverteerd worden naar tekst (JSON.parse() verwijderd alle methodes uit een klasse)
+//hiervoor maken we een klasse aan die de interface implementeert
+
+export class Task implements ITask{
+    done: boolean;
+    id: number;
+    name: string;
+
+    
+    constructor(obj: ITask){
+        Object.assign(this, obj)
+    }
+    //dit is een copy-constructor (doet niets anders dan gegevens kopiëren van een bestaand object naar de nieuwe instantie van het object dat de constructor bevat)
+}
