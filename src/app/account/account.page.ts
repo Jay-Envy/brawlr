b0ebdfd49b2c8f8c6b3b205f61d5a2f80b2c9ec4
Jelle Vermeulen <jelle.vermeulen94@gmail.com>
2022-08-05 17:33:06 +0200

    cam android not perfect

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -6,20 +6,8 @@ import { AppComponent } from '../app.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicRouteStrategy } from '@ionic/angular';
-import { CamService } from '../services/cam.service';
+import { PhotoService } from '../services/cam.service';
 
-@NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [AppRoutingModule,
-  //Firebase main import.
-provideFirebaseApp(()=> initializeApp(environment.firebaseConfig)),
-//Firebase authentication import.
-provideAuth(()=> getAuth())
-],
-providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
-bootstrap: [AppComponent],
-})
 
 @Component({
   selector: 'app-account',
@@ -28,6 +16,6 @@ bootstrap: [AppComponent],
 })
 export class AccountPage {
 
-  constructor(public camService: CamService) {}
+  constructor(public photoService: PhotoService) {}
 
 }
