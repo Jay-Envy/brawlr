c8f01b13d35befae328cd0a49361887915337765
Jelle Vermeulen <jelle.vermeulen94@gmail.com>
2022-08-10 14:45:44 +0200

    trying to get firebase to work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {redirectUnauthorizedTo, AuthGuard} from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['account']);
 
 const routes: Routes = [
   {
@@ -10,6 +13,22 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canActivate: [AuthGuard],
+    data: {authGuardPipe: redirectUnauthorizedToLogin}
+  },
+  {
+    path: 'brawl',
+    loadChildren: () => import('./brawl/brawl.module').then( m => m.BrawlPageModule),
+    canActivate: [AuthGuard],
+    data: {authGuardPipe: redirectUnauthorizedToLogin}
   },
 ];
 @NgModule({
